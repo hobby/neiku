b3c88ea1dd5e4695f890fdf8809cdf3f0543d896
hobby <ku7d@qq.com>
2015-05-05 23:00:42 +0800

    alias/u2g/g2u, git config

---
+++ config/git/git.config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# git config
+
+# colorful
+git config --global color.ui true
+
+# alias
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.co checkout
+
+
+
