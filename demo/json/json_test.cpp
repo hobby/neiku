762b0def23ca3ee53f924493f154a354face7147
hobby <ku7d@qq.com>
2015-05-30 22:55:06 +0800

    add json (jsoncpp.[h/cpp] 0.10.2) for neiku

---
+++ demo/json/json_test.cpp
@@ -0,0 +1,31 @@
+
+// vim:fenc=utf-8:ff=unix:ts=4:sw=4:et
+// desc: jsoncpp test.
+
+#include <stdint.h>
+#include <cstdio>
+#include <string>
+
+#include <neiku/json.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    std::string jsonstr = "{\"name\":\"名字\",\"gender\":\"性别\", \"\":\"empty value\"}";
+    printf("jsonstr:[%s]\n", jsonstr.c_str());
+
+    Json::Reader reader;
+    Json::Value json, defaultString("默认值");
+    if (reader.parse(jsonstr, json))
+    {
+        printf("json:[%s]\n", json.toStyledString().c_str());
+
+        std::string name = json["name"].asString();
+        std::string gender = json["gender"].asString();
+        std::string empty = json[""].asString();
+        printf("json.name:[%s], json.gender:[%s], empty:[%s]\n"
+               , name.c_str(), gender.c_str(), empty.c_str());
+    }
+    return 0;
+}
---
+++ include/neiku/json.h
@@ -0,0 +1,25 @@
+
+/*
+ * file:   neiku/json.h
+ * desc:   json tools for C++ base on JsonCPP
+ * author: hobby
+ * date:   2015/05/30 22:49:00
+ *
+ * usage:
+ *       #include <neiku/json.h>
+ *
+ * compile: -lneiku
+ *
+ * vim:ts=4;sw=4;expandtab
+ */
+
+#ifndef __NK_JSON_H__
+#define __NK_JSON_H__ 1
+
+#include <neiku/jsoncpp.h>
+
+namespace neiku
+{
+};
+
+#endif
