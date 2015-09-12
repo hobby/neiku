f57a5fb380038bbfedae5a967f530acb6b4ed0c1
hobby <ku7d@qq.com>
2015-09-12 10:56:54 +0800

    serialize工具: v7-单行json和格式化json由两个独立的工具生成

--- include/neiku/json.h
+++ include/neiku/json.h
@@ -20,6 +20,8 @@
  *              支持json格式化(添加换行、缩进)，提高可读性
  *          v6  -- json encoder
  *              支持bool类型
+ *          v7  -- json encoder
+ *              区分单行json和格式化json生成工具，不要撸在一起
  * usage:
  *       #include "neiku/json.h"
  *
@@ -43,8 +45,8 @@
 #define SERIALIZE(ar, obj) { ar & #obj & obj; }
 #endif
 
-#define json_encode(obj)      ((neiku::CJsonEncoder(false) << obj).str().c_str())
-#define json_encode_ml(obj)   ((neiku::CJsonEncoder(true) << obj).str().c_str())
+#define json_encode(obj)      ((neiku::CJsonEncoder() << obj).str().c_str())
+#define json_encode_ml(obj)   ((neiku::CJsonEncoderMl() << obj).str().c_str())
 
 namespace neiku
 {
@@ -53,9 +55,7 @@ namespace neiku
 class CJsonEncoder
 {
     public:
-        CJsonEncoder(bool bMultiLine = false): m_bIsFirstKey(false)
-                                             , m_bMultiLine(bMultiLine)
-                                             , m_iIndentNum(0)
+        CJsonEncoder(): m_bIsFirstKey(false)
         {};
 
         // 返回json
@@ -74,29 +74,14 @@ class CJsonEncoder
 	public:
         CJsonEncoder& operator & (const char* szKeyName)
         {
-            if (!m_bMultiLine)
+            if (m_bIsFirstKey)
             {
-                if (m_bIsFirstKey)
-                {
-                    m_bIsFirstKey = false;
-                    m_ssJson << "\"" << szKeyName << "\":";
-                }
-                else
-                {
-                    m_ssJson << ",\"" << szKeyName << "\":";
-                }
+                m_bIsFirstKey = false;
+                m_ssJson << "\"" << szKeyName << "\":";
             }
             else
             {
-                if (m_bIsFirstKey)
-                {
-                    m_bIsFirstKey = false;
-                    m_ssJson << "\n" << Indent() << "\"" << szKeyName << "\": ";
-                }
-                else
-                {
-                    m_ssJson << "," << "\n" << Indent() << "\"" << szKeyName << "\": ";
-                }
+                m_ssJson << ",\"" << szKeyName << "\":";
             }
             return *this;
         }
@@ -149,42 +134,18 @@ class CJsonEncoder
         template <class T>
         CJsonEncoder& operator & (std::vector<T>& vector)
         {
-            if(!m_bMultiLine)
+            m_ssJson << "[";
+            if (!vector.empty())
             {
-                m_ssJson << "[";
-                if (!vector.empty())
+                size_t index = 0;
+                *this & vector[index];
+                for (++index; index < vector.size(); ++index)
                 {
-                    size_t index = 0;
+                    m_ssJson << ",";
                     *this & vector[index];
-                    for (++index; index < vector.size(); ++index)
-                    {
-                        m_ssJson << ",";
-                        *this & vector[index];
-                    }
                 }
-                m_ssJson << "]";
             }
-            else
-            {
-                m_ssJson << "[";
-                if (!vector.empty())
-                {
-                    int i  = m_iIndentNum;
-                    m_iIndentNum += 1;
-                    m_ssJson << "\n" << Indent();
-                    size_t index = 0;
-                    *this & vector[index];
-                    for (++index; index < vector.size(); ++index)
-                    {
-                        m_ssJson << "," << "\n" << Indent();
-                        *this & vector[index];
-                    }
-                    m_iIndentNum -= 1;
-                    m_ssJson << "\n" << Indent();
-                    m_iIndentNum = i;
-                }
-                m_ssJson << "]";
-            }
+            m_ssJson << "]";
             return *this;
         }
 
@@ -192,53 +153,24 @@ class CJsonEncoder
         template <class KEY, class VALUE>
         CJsonEncoder& operator & (std::map<KEY, VALUE>& map)
         {
-            if(!m_bMultiLine)
+            m_ssJson << "{";
+            if (!map.empty())
             {
-                m_ssJson << "{";
-                if (!map.empty())
+                typename std::map<KEY, VALUE>::iterator it = map.begin();
+                std::stringstream ss;
+                ss << it->first;
+                m_ssJson << Json::valueToQuotedString(ss.str().c_str()) << ":";
+                *this & it->second;
+                for (++it; it != map.end(); ++it)
                 {
-                    typename std::map<KEY, VALUE>::iterator it = map.begin();
-                    std::stringstream ss;
+                    // std::stringstream::str("") 才是真正的清空方法
+                    ss.str("");
                     ss << it->first;
-                    m_ssJson << Json::valueToQuotedString(ss.str().c_str()) << ":";
+                    m_ssJson << "," << Json::valueToQuotedString(ss.str().c_str()) << ":";
                     *this & it->second;
-                    for (++it; it != map.end(); ++it)
-                    {
-                        // std::stringstream::str("") 才是真正的清空方法
-                        ss.str("");
-                        ss << it->first;
-                        m_ssJson << "," << Json::valueToQuotedString(ss.str().c_str()) << ":";
-                        *this & it->second;
-                    }
                 }
-                m_ssJson << "}";
             }
-            else
-            {
-                m_ssJson << "{";
-                if (!map.empty())
-                {
-                    int i  = m_iIndentNum;
-                    m_iIndentNum += 1;
-                    typename std::map<KEY, VALUE>::iterator it = map.begin();
-                    std::stringstream ss;
-                    ss << it->first;
-                    m_ssJson << "\n" << Indent() << Json::valueToQuotedString(ss.str().c_str()) << ": ";
-                    *this & it->second;
-                    for (++it; it != map.end(); ++it)
-                    {
-                        // std::stringstream::str("") 才是真正的清空方法
-                        ss.str("");
-                        ss << it->first;
-                        m_ssJson << "," << "\n" << Indent() << Json::valueToQuotedString(ss.str().c_str()) << ": ";
-                        *this & it->second;
-                    }
-                    m_iIndentNum -= 1;
-                    m_ssJson << "\n" << Indent();
-                    m_iIndentNum  = i;
-                }
-                m_ssJson << "}";
-            }
+            m_ssJson << "}";
             return *this;
         }
 
@@ -246,32 +178,179 @@ class CJsonEncoder
         template <class T>
         CJsonEncoder& operator & (T& o)
         {
-            if(!m_bMultiLine)
-            {
-                bool b = m_bIsFirstKey;
-                m_bIsFirstKey = true;
-                m_ssJson << "{";
-                o.serialize(*this);
-                m_ssJson << "}";
-                m_bIsFirstKey = b;
-            }
-            else
-            {
-                bool b = m_bIsFirstKey;
-                int i  = m_iIndentNum;
-                m_bIsFirstKey = true;
-                m_ssJson << "{";
-                m_iIndentNum += 1;
-                o.serialize(*this);
-                m_iIndentNum -= 1;
-                m_ssJson << "\n" << Indent() << "}";
-                m_bIsFirstKey = b;
-                m_iIndentNum  = i;
-            }
+            bool b = m_bIsFirstKey;
+            m_bIsFirstKey = true;
+            m_ssJson << "{";
+            o.serialize(*this);
+            m_ssJson << "}";
+            m_bIsFirstKey = b;
             return *this;
         }
         template <class T>
         CJsonEncoder& operator & (const T& o)
+        {
+            // 兼容const obj
+            return *this & const_cast<T&>(o);
+        }
+
+    private:
+        bool m_bIsFirstKey;
+        std::stringstream m_ssJson;
+};
+
+// JsonEncoderMl -- 支持C++对象自动生成格式化JSON对象（自动填充换行、缩进）
+class CJsonEncoderMl
+{
+    public:
+        CJsonEncoderMl(): m_bIsFirstKey(false)
+                        , m_iIndentNum(0)
+        {};
+
+        // 返回json
+        std::string str()
+        {
+            return m_ssJson.str();
+        }
+
+        // json encode
+        template <class T>
+        CJsonEncoderMl& operator << (const T& o)
+        {
+            return *this & o;
+        }
+
+    public:
+        CJsonEncoderMl& operator & (const char* szKeyName)
+        {
+            if (m_bIsFirstKey)
+            {
+                m_bIsFirstKey = false;
+                m_ssJson << "\n" << Indent() << "\"" << szKeyName << "\": ";
+            }
+            else
+            {
+                m_ssJson << "," << "\n" << Indent() << "\"" << szKeyName << "\": ";
+            }
+            return *this;
+        }
+
+        CJsonEncoderMl& operator & (bool b)
+        {
+            if (b == true)
+            {
+                m_ssJson << "true";
+            }
+            else
+            {
+                m_ssJson << "false";
+            }
+            return *this;
+        }
+
+        CJsonEncoderMl& operator & (int32_t i32)
+        {
+            m_ssJson << i32;
+            return *this;
+        }
+
+        CJsonEncoderMl& operator & (int64_t i64)
+        {
+            m_ssJson << i64;
+            return *this;
+        }
+
+        CJsonEncoderMl& operator & (uint32_t u32)
+        {
+            m_ssJson << u32;
+            return *this;
+        }
+
+        CJsonEncoderMl& operator & (uint64_t u64)
+        {
+            m_ssJson << u64;
+            return *this;
+        }
+
+        // 字符串两边由对引号包住
+        CJsonEncoderMl& operator & (std::string& str)
+        {
+             m_ssJson << Json::valueToQuotedString(str.c_str());
+            return *this;
+        }
+
+        // vector 视为 json 数组
+        template <class T>
+        CJsonEncoderMl& operator & (std::vector<T>& vector)
+        {
+            m_ssJson << "[";
+            if (!vector.empty())
+            {
+                int i  = m_iIndentNum;
+                m_iIndentNum += 1;
+                m_ssJson << "\n" << Indent();
+                size_t index = 0;
+                *this & vector[index];
+                for (++index; index < vector.size(); ++index)
+                {
+                    m_ssJson << "," << "\n" << Indent();
+                    *this & vector[index];
+                }
+                m_iIndentNum -= 1;
+                m_ssJson << "\n" << Indent();
+                m_iIndentNum = i;
+            }
+            m_ssJson << "]";
+            return *this;
+        }
+
+        // map 视为 json 对象
+        template <class KEY, class VALUE>
+        CJsonEncoderMl& operator & (std::map<KEY, VALUE>& map)
+        {
+            m_ssJson << "{";
+            if (!map.empty())
+            {
+                int i  = m_iIndentNum;
+                m_iIndentNum += 1;
+                typename std::map<KEY, VALUE>::iterator it = map.begin();
+                std::stringstream ss;
+                ss << it->first;
+                m_ssJson << "\n" << Indent() << Json::valueToQuotedString(ss.str().c_str()) << ": ";
+                *this & it->second;
+                for (++it; it != map.end(); ++it)
+                {
+                    // std::stringstream::str("") 才是真正的清空方法
+                    ss.str("");
+                    ss << it->first;
+                    m_ssJson << "," << "\n" << Indent() << Json::valueToQuotedString(ss.str().c_str()) << ": ";
+                    *this & it->second;
+                }
+                m_iIndentNum -= 1;
+                m_ssJson << "\n" << Indent();
+                m_iIndentNum  = i;
+            }
+            m_ssJson << "}";
+            return *this;
+        }
+
+        // 自定义对象 视为 json 对象
+        template <class T>
+        CJsonEncoderMl& operator & (T& o)
+        {
+            bool b = m_bIsFirstKey;
+            int i  = m_iIndentNum;
+            m_bIsFirstKey = true;
+            m_ssJson << "{";
+            m_iIndentNum += 1;
+            o.serialize(*this);
+            m_iIndentNum -= 1;
+            m_ssJson << "\n" << Indent() << "}";
+            m_bIsFirstKey = b;
+            m_iIndentNum  = i;
+            return *this;
+        }
+        template <class T>
+        CJsonEncoderMl& operator & (const T& o)
         {
             // 兼容const obj
             return *this & const_cast<T&>(o);
@@ -286,8 +365,6 @@ class CJsonEncoder
     private:
         bool m_bIsFirstKey;
         std::stringstream m_ssJson;
-
-        bool m_bMultiLine;
         int  m_iIndentNum;
 };
 
