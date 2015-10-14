5dc24b0fffcb380da107c4e067d1f99eb0a94158
hobby <ku7d@qq.com>
2015-10-14 15:07:27 +0800

    git: 使用br取代bh

--- config/git/git.config.sh
+++ config/git/git.config.sh
@@ -9,7 +9,7 @@ git config --global color.ui true
 git config --global alias.ci 'commit'
 git config --global alias.st 'status'
 git config --global alias.co 'checkout'
-git config --global alias.bh 'branch -v'
+git config --global alias.br 'branch -av'
 git config --global alias.lo 'log --pretty=oneline --abbrev-commit'
 git config --global alias.lg 'log --pretty=oneline --graph --abbrev-commit'
 
