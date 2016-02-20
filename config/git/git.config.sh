f61e33e1b8331fa44c81493b6df75b34e6aac7fd
YIF <ku7d@qq.com>
2016-02-20 14:28:10 +0800

    git: git-pushex - enable pre-push/post-push hooks for git-push

--- config/git/git.config.sh
+++ config/git/git.config.sh
@@ -12,6 +12,9 @@ git config --global alias.co 'checkout'
 git config --global alias.br 'branch -v'
 git config --global alias.lo 'log --pretty=oneline --abbrev-commit --decorate'
 git config --global alias.lg 'log --pretty=oneline --graph --abbrev-commit --decorate'
+git config --global alias.fh 'fetch'
+git config --global alias.pl 'pull'
+git config --global alias.ps 'push'
 
 # 解决中文名称显示成"\xxx\xxx\xxx"的问题
 git config --global core.quotepath false
