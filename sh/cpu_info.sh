749aa67d81baefa00d54cba2bc4e1d05aff20937
YIF <ku7d@qq.com>
2016-03-29 20:45:27 +0800

    cpu: cpu_info.sh v1.1 - 支持显示cpu线程数、缓存参数

--- sh/cpu_info.sh
+++ sh/cpu_info.sh
@@ -13,6 +13,8 @@ cpu_name=`cat /proc/cpuinfo | grep "model name" | head -1 | cut -d: -f2 | cut -c
 physical_cpu_num=`cat /proc/cpuinfo | grep "physical id" | sort -u | wc -l`
 logical_cpu_num=`cat /proc/cpuinfo | grep "processor" | sort -u | wc -l`
 cpu_core_num=`cat /proc/cpuinfo | grep "cpu cores" | head -1 | cut -f2 -d: | tr -d ' '`
+cpu_siblings_num=`cat /proc/cpuinfo | grep "siblings" | head -1 | cut -f2 -d: | tr -d ' '`
+cpu_cache_size_num=`cat /proc/cpuinfo | grep "cache size" | head -1 | cut -f2 -d: | tr -d ' '`
 isenableht="unknow"
 
 
@@ -20,6 +22,8 @@ echo "           cpu name: $cpu_name"
 echo "physical cpu number: $physical_cpu_num"
 echo " logical cpu number: $logical_cpu_num"
 echo "    cpu core number: $cpu_core_num"
+echo "cpu siblings number: $cpu_siblings_num"
+echo "     cpu cache size: $cpu_cache_size_num"
 if ((physical_cpu_num*cpu_core_num < logical_cpu_num)) ; then
     isenableht="yes"
 else
